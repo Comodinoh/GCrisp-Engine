853eb45703da34095dd8b321a2d4f8401c5d6617
comodinoh <plumino3124@gmail.com>
2024-11-09 22:51:29 +0200

    integrated gcrisp library with SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <core.h>
+#include <GCrisp/core.h>
+#include <SDL3/SDL.h>
 
-
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
   GCrispCoreInfo* core_info = gcrisp_core_start("asd", 1);
+  SDL_Log("Meow");
   return 0;
 }
